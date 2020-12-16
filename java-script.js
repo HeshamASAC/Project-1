e1efafff5730eb596db3cbfc695c6a45da6a8a2c
Hesham Mashriqi <75928288+HeshamASAC@users.noreply.github.com>
2020-12-16 19:55:10 +0200

    Update java-script.js

--- java-script.js
+++ java-script.js
@@ -5,15 +5,3 @@ var pass=prompt("password?");
 alert("Done By: Hesham Mashriqi");
 
 
-
-
-String pass;
-String user;
-user = txtUser.getText();
-pass = txtPass.getText();
-
-if(pass.equals("blue") && user.equals("bob") ){
-    lblDisplay.setText("Credentials Accepted.");
-}
-else{
-    lblDisplay.setText("Please try again.");

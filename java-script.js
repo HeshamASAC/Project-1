de6b08921ef1cca8d912922d6f8a1b10b1c3bedd
Hesham Mashriqi <75928288+HeshamASAC@users.noreply.github.com>
2020-12-22 20:08:08 +0200

    Update java-script.js

--- java-script.js
+++ java-script.js
@@ -1,7 +1,78 @@
-varName=prompt("username");
+//  varName=prompt("username");
 
-varpass=prompt("password?");
+//  varpass=prompt("password?");
 
-alert("Done By: Hesham Mashriqi");
+//  alert("Done By: Hesham Mashriqi");
+
+// var number = 10
+
+// CheckNumbers();
+// // var number = 10;
 
 
+// // if(number > 10){
+
+// //     alert("correct");
+
+// // } else { alert("not correct")}
+
+
+// function CheckNumbers() {
+
+
+//     if(number > 10){
+    
+//         alert("correct");
+    
+//     } else { alert("not correct")}
+    
+// }
+
+
+
+// var names = prompt("whats your name?");
+
+// alert("Hello" + names);
+
+
+
+ var name = prompt("What is your name?");
+ var pass = prompt("What is your password?");
+
+checkPassword(pass);
+
+
+
+
+function printName(name) {
+    alert("Hello "+name);
+}
+
+function checkPassword(password){
+    if (password == 1234) {
+        alert("Logged in");
+        printName(name);
+    } else {
+        alert("password not correct");
+
+
+    }
+
+
+}
+
+
+
+var limit = 25;
+
+for (var i = 0; i < limit; i++) {
+    console.log(i);
+}
+
+var myCondition = true;
+
+while (myCondition) {
+
+    console.log("MY condition is " + myCondition);
+    myCondition = false;
+}
